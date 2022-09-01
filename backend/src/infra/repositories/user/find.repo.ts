98fd714c14dbbf2f659293bbfe51a89bef462045
henrique <henrique.multitech@gmail.com>
2022-09-01 14:34:34 -0300

    feat: Implements and construct roles for passed tests

---
+++ backend/src/infra/repositories/user/find.repo.ts
@@ -0,0 +1,35 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { IFindUserById, IFindUserByLogin, IFindUserRepo } from '@root/domain';
+import { PrismaService } from '@root/infra';
+
+@Injectable()
+class FindUserRepo implements IFindUserRepo {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly logger: Logger,
+  ) {}
+
+  async by_login({
+    login,
+  }: IFindUserByLogin.Params): IFindUserByLogin.Response {
+    try {
+      const user = await this.prisma.user.findFirst({ where: { login } });
+      return user;
+    } catch (err) {
+      this.logger.log('Erro ao buscar usuario no banco de dados...');
+      this.logger.error(err.message);
+    }
+  }
+
+  async by_id({ user_id }: IFindUserById.Params): IFindUserById.Response {
+    try {
+      const user = await this.prisma.user.findFirst({ where: { user_id } });
+      return user;
+    } catch (err) {
+      this.logger.log('Erro ao buscar usuario no banco de dados...');
+      this.logger.error(err.message);
+    }
+  }
+}
+
+export { FindUserRepo };
