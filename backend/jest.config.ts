0cfb16443df6bfe4f9f33b12a44c6ba5bb82feef
henrique <henrique.multitech@gmail.com>
2022-09-01 10:30:29 -0300

    refactor: re-config absolute paths for tests

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -14,13 +14,14 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/setup/setup.ts'],
   setupFiles: ['dotenv/config'],
   moduleNameMapper: {
-    '@root/(.*)': ['src/(.*)'],
-    '@contracts/(.*)': ['src/domain/$1'],
-    '@impl/(.*)': ['src/data/$1'],
-    '@usecases/(.*)': ['src/data/usecases/$1'],
-    '@resolvers/(.*)': ['src/presentation/resolvers/$1'],
-    '@utils/(.*)': ['src/shared/$1'],
-    '@core/(.*)': ['src/core/$1'],
+    '@root/(.*)': '<rootDir>/src/$1',
+    '@contracts/(.*)': '<rootDir>/src/domain/$1',
+    '@impl/(.*)': '<rootDir>/src/data/$1',
+    '@usecases/(.*)': '<rootDir>/src/data/usecases/$1',
+    '@resolvers/(.*)': '<rootDir>/src/presentation/resolvers/$1',
+    '@utils/(.*)': '<rootDir>/src/shared/$1',
+    '@core/(.*)': '<rootDir>/src/core/$1',
+    '@testRoot/(.*)': '<rootDir>/test/$1',
   },
 };
 // globalSetup: '<rootDir>/__tests__/setup/global-setup.ts'
