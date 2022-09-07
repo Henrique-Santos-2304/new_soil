8760650634e50dbc6183ddf0bc21bba1b927efb0
Henrique dos Santos <henrique.multitech@gmail.com>
2022-09-07 10:49:45 -0300

    feat: Mutation createUser yet impl

--- backend/src/presentation/index.ts
+++ backend/src/presentation/index.ts
@@ -2,3 +2,4 @@
     Aqui teremos nossas camadas mais próxima do usuario, a camada de controle das
     requisições, sejam elas Resolvers(Graphql) ou Controlers(Rest Api), Rotas estarão aqui
 */
+export * from './resolvers';
---
+++ backend/src/presentation/resolvers/users/create-user.resolver.ts
@@ -0,0 +1,39 @@
+import { Inject, Logger } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateUserDto,
+  ICreateUserController,
+  ICreateUserService,
+} from '@root/domain';
+
+@Resolver()
+class CreateUserResolver implements ICreateUserController {
+  constructor(
+    @Inject('ICreateUserService')
+    private readonly createUserService: ICreateUserService,
+  ) {}
+
+  /* Provisório só está para o graphql não reclamar de falta de query*/
+  @Query()
+  async getUserByLogin(@Args('login') login: string) {
+    return '';
+  }
+
+  @Mutation()
+  async createUser(
+    @Args('data') data: ICreateUserController.Params,
+  ): ICreateUserController.Response {
+    Logger.log(
+      `Recebido novo Usuario para cadastro... ${JSON.stringify(data)} `,
+    );
+    try {
+      const user = await this.createUserService.start(data);
+      Logger.log(`Usuario cadastrado com sucesso...\n`);
+      return user;
+    } catch (err) {
+      return { status: 'Fail', error: err.message };
+    }
+  }
+}
+
+export { CreateUserResolver };
