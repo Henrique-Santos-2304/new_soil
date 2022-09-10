24117319fbb169abd0ceafb961e5e9ab571b146b
Henrique dos Santos <henrique.multitech@gmail.com>
2022-09-10 18:05:25 -0300

    refactor: add comment for docummentatio contracts to ITokenService and IAuthUserController

--- backend/src/domain/controllers/users/auth-user.controller.domain.ts
+++ backend/src/domain/controllers/users/auth-user.controller.domain.ts
@@ -1,4 +1,26 @@
 import { CreateUserDto } from '@root/domain/models';
+/* Autenticação de usuario via login
+  *****************************************************************
+    @Params: {
+      login: string, 
+      password: string, 
+    }
+    
+  *****************************************************************
+    @Response: 
+      Em caso de sucesso: {
+        status: 'Sucess', token: string
+      }, 
+      Em caso de erro: {
+        status: 'Fail',
+        error: string
+        }, 
+      Em caso de erro do Graphql: {
+        errors: Array<{message: string}>
+      }
+
+  ******************************************************************
+*/
 
 interface IAuthUserController {
   authUser(data: IAuthUserController.Params): IAuthUserController.Response;
--- backend/src/domain/validators/token.domain.ts
+++ backend/src/domain/validators/token.domain.ts
@@ -1,5 +1,29 @@
 import { UserModel } from '../models';
 
+/*
+  Serviço geração e comparação de Token Jwt
+
+  ******************************************************************************
+
+  @Params
+    generate = { 
+      user_id: string , 
+      userType: string 
+    } 
+    checkIsValid = {
+      token: string
+    }
+
+  @Response
+    generate = 
+      Em caso de erro = new Error('TOKEN ERROR')
+      Em caso de sucesso = string com token criptogrado
+
+    checkIsValid = 
+      Em caso de erro = new Error('TOKEN ERROR')
+      Em caso de sucesso = true
+
+*/
 interface ITokenService {
   generate(data: IGenerateUserToken.Params): IGenerateUserToken.Response;
   checkIsValid({ token }: ICheckTokenValid.Params): ICheckTokenValid.Response;
