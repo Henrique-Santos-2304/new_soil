582b89b485acd489e113182d112eaa0c892e73fb
Henrique dos Santos <henrique.multitech@gmail.com>
2022-09-10 16:44:13 -0300

    refactor: Refactor user-service, to be split all logics into specific method

--- backend/src/data/usecases/users/auth-user.service.ts
+++ backend/src/data/usecases/users/auth-user.service.ts
@@ -4,37 +4,54 @@ import {
   IEncrypterData,
   IFindUserRepo,
   ITokenService,
+  UserModel,
 } from '@contracts/index';
 
 @Injectable()
 class AuthUserService implements IAuthUserService {
+  private user: Omit<UserModel, 'login'>;
+  private token: string;
+
   constructor(
     @Inject('IFindUserRepo') private readonly findUserRepo: IFindUserRepo,
     @Inject('IEncrypterData') private readonly encrypter: IEncrypterData,
     @Inject('ITokenService') private readonly tokenService: ITokenService,
   ) {}
 
-  async start({
-    login,
-    password,
-  }: IAuthUserService.Params): IAuthUserService.Response {
+  async checkUserAlreadyExists(login: string): Promise<void> {
     const user = await this.findUserRepo.without_login({ login });
 
     if (!user) throw new Error('Invalid Credentials');
+    else this.user = user;
+  }
 
-    const { password: userPassword, userType, user_id } = user;
-
+  async checkPasswordIsValid(password: string): Promise<void> {
     const passwordIsValid = await this.encrypter.compare({
-      old_value: userPassword,
+      old_value: this.user.password,
       valueCompare: password,
     });
 
     if (!passwordIsValid) throw new Error('Invalid Credentials');
+  }
+
+  async generateUserToken(): Promise<void> {
+    const { userType, user_id } = this.user;
 
     const { token } = await this.tokenService.generate({ user_id, userType });
 
     if (!token) throw new Error('TOKEN DOES NOT PROVIDED');
-    return { status: 'Sucess', token };
+    else this.token = token;
+  }
+
+  async start({
+    login,
+    password,
+  }: IAuthUserService.Params): IAuthUserService.Response {
+    await this.checkUserAlreadyExists(login);
+    await this.checkPasswordIsValid(password);
+    await this.generateUserToken();
+
+    return { status: 'Sucess', token: this.token };
   }
 }
 
