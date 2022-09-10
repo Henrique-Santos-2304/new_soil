aa2daa0b5aa8bda513a357aa4718f4eadcc23b71
Henrique dos Santos <henrique.multitech@gmail.com>
2022-09-10 12:06:45 -0300

    feat: Add Find User without login and password contract to IFindUserRepo Domain

--- backend/src/domain/repositories/users/find.repo.domain.ts
+++ backend/src/domain/repositories/users/find.repo.domain.ts
@@ -8,18 +8,23 @@ import { CreateUserDto, UserModel } from '@root/domain/models';
   @Params: 
       by_login = { login }
       by_id = { user_id }
+      without_login_pass = { login}
+      
     
   *****************************************************************
     @Response: 
       Em caso de erro retornar = new Error("QUERY ERROR")
       Em caso de achar o usuario = retorna todos os dados do usuario<UserModel> com a senha criptografada
-
+      without_login_pass = Retorna userId e userType do usuario somente 
   ******************************************************************
 
 */
 interface IFindUserRepo {
   by_login({ login }: IFindUserByLogin.Params): IFindUserByLogin.Response;
   by_id({ user_id }: IFindUserById.Params): IFindUserById.Response;
+  without_login_pass({
+    login,
+  }: IFindUserWithoutLoginPassword.Params): IFindUserWithoutLoginPassword.Response;
 }
 
 namespace IFindUserByLogin {
@@ -27,9 +32,21 @@ namespace IFindUserByLogin {
   export type Response = Promise<UserModel | undefined>;
 }
 
+namespace IFindUserWithoutLoginPassword {
+  export type Params = { login: CreateUserDto['login'] };
+  export type Response = Promise<
+    Omit<UserModel, 'login' | 'password'> | undefined
+  >;
+}
+
 namespace IFindUserById {
   export type Params = { user_id: UserModel['user_id'] };
   export type Response = Promise<UserModel | undefined>;
 }
 
-export { IFindUserRepo, IFindUserByLogin, IFindUserById };
+export {
+  IFindUserRepo,
+  IFindUserByLogin,
+  IFindUserById,
+  IFindUserWithoutLoginPassword,
+};
