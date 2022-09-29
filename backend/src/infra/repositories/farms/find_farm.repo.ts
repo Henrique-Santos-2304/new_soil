1d0dbb7954fd92e266c9ede48a57dde37d225d49
Henrique dos Santos <henrique.multitech@gmail.com>
2022-09-29 15:21:51 -0300

    feat: create repo find farms and passed test

--- backend/src/infra/repositories/farms/find_farm.repo.ts
+++ backend/src/infra/repositories/farms/find_farm.repo.ts
@@ -4,18 +4,52 @@ import {
   IFindFarmsRepo,
   IGetAllFarms,
 } from '@contracts/index';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '@root/infra';
 
 @Injectable()
 class FindFarmRepo implements IFindFarmsRepo {
-  by_user({ user_id }: IFindFarmsByUser.Params): IFindFarmsByUser.Response {
-    throw new Error('Method not implemented.');
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly logger: Logger,
+  ) {}
+
+  async by_user({
+    user_id,
+  }: IFindFarmsByUser.Params): IFindFarmsByUser.Response {
+    try {
+      const farms = await this.prisma.farm.findMany({
+        where: { admins: { some: { adminId: user_id } } },
+      });
+
+      return farms;
+    } catch (err) {
+      this.logger.log('Erro ao buscar fazenda no banco de dados...');
+      this.logger.error(err.message);
+      throw new Error('QUERY_ERROR');
+    }
   }
-  by_id({ farm_id }: IFindFarmById.Params): IFindFarmById.Response {
-    throw new Error('Method not implemented.');
+
+  async by_id({ farm_id }: IFindFarmById.Params): IFindFarmById.Response {
+    try {
+      const farm = await this.prisma.farm.findFirst({ where: { farm_id } });
+      return farm;
+    } catch (err) {
+      this.logger.log('Erro ao buscar fazenda no banco de dados...');
+      this.logger.error(err.message);
+      throw new Error('QUERY_ERROR');
+    }
   }
-  all(): IGetAllFarms.Response {
-    throw new Error('Method not implemented.');
+
+  async all(): IGetAllFarms.Response {
+    try {
+      const farms = await this.prisma.farm.findMany();
+      return farms;
+    } catch (err) {
+      this.logger.log('Erro ao buscar fazenda no banco de dados...');
+      this.logger.error(err.message);
+      throw new Error('QUERY_ERROR');
+    }
   }
 }
 
