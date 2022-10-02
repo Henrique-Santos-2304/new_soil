3857d94141f365f2b113ac7b906b91bc36b0c8a6
Henrique dos Santos <henrique.multitech@gmail.com>
2022-10-02 00:29:39 -0300

    refactor: Create named methods for single responsability

--- backend/src/data/usecases/farms/create-farm.service.ts
+++ backend/src/data/usecases/farms/create-farm.service.ts
@@ -4,6 +4,7 @@ import {
   ICreateFarmService,
   IFindFarmsRepo,
   IFindUserRepo,
+  UserModel,
 } from '@contracts/index';
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 
@@ -14,29 +15,40 @@ class CreateFarmService implements ICreateFarmService {
     @Inject('IFindUserRepo') private readonly findUserRepo: IFindUserRepo,
     @Inject('ICreateFarmRepo') private readonly createFarmRepo: ICreateFarmRepo,
   ) {}
-  async start(farm: CreateFarmDTO): ICreateFarmService.Response {
-    const farmAlreadyExists = await this.findFarmRepo.by_id({
-      farm_id: farm.farm_id,
-    });
+
+  async checkFarmAlreadyExistsInDb(farm_id: string): Promise<void> {
+    const farmAlreadyExists = await this.findFarmRepo.by_id({ farm_id });
 
     if (farmAlreadyExists) throw new Error('Farm Already Exists');
+  }
 
-    const userExists = await this.findUserRepo.by_id({
-      user_id: farm.created_by,
-    });
+  async checkUserCreatorExistsInDb(user_id: string): Promise<UserModel> {
+    const userExists = await this.findUserRepo.by_id({ user_id });
 
     if (!userExists) throw new Error('Does Not Found User');
 
-    console.log(userExists);
-    if (userExists.userType === 'USER' || userExists.userType === 'ADMIN') {
-      console.log('Unauthorized User');
+    return userExists;
+  }
+
+  verifyUserToHaveAcessForCreateFarm(userType: string): void {
+    if (userType !== 'MASTER' && userType !== 'DEALER') {
       throw new UnauthorizedException();
     }
+  }
 
+  async createNewFarmInDb(farm: CreateFarmDTO): Promise<{ farm_id: string }> {
     const createdUser = await this.createFarmRepo.create(farm);
 
     if (!createdUser) throw new Error('Does not possible to create a new farm');
-    return { status: 'Sucess', farm_id: createdUser.farm_id };
+    return createdUser;
+  }
+
+  async start(farm: CreateFarmDTO): ICreateFarmService.Response {
+    await this.checkFarmAlreadyExistsInDb(farm.farm_id);
+    const { userType } = await this.checkUserCreatorExistsInDb(farm.created_by);
+    this.verifyUserToHaveAcessForCreateFarm(userType);
+    const { farm_id } = await this.createNewFarmInDb(farm);
+    return { status: 'Sucess', farm_id };
   }
 }
 
