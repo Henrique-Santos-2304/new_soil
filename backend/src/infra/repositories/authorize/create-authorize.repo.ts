03319121aa98be6493d807b094a5d10de8310c6e
Henrique dos Santos <henrique.multitech@gmail.com>
2022-10-17 21:54:22 -0300

    feat: implement repository of create authorize

---
+++ backend/src/infra/repositories/authorize/create-authorize.repo.ts
@@ -0,0 +1,26 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CreateAuthorizeDto } from '@root/domain';
+import { ICreateAuthorizeRepo } from '@root/domain/repositories/authorize/create-authorize.domain';
+import { PrismaService } from '@root/infra';
+
+@Injectable()
+class CreateAuthorizeRepo implements ICreateAuthorizeRepo {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly logger: Logger,
+  ) {}
+  async create(authorize: CreateAuthorizeDto): ICreateAuthorizeRepo.Response {
+    try {
+      return await this.prisma.authorize.create({
+        data: { ...authorize },
+        select: { authorize_id: true },
+      });
+    } catch (err) {
+      this.logger.log('Erro ao criar nova authorização...');
+      this.logger.error(err.message);
+      throw new Error('QUERY_ERROR');
+    }
+  }
+}
+
+export { CreateAuthorizeRepo };
